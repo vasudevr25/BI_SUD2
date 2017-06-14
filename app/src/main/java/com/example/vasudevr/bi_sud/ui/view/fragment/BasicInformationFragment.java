03f8e1e487071c28736bfebe4f6ce1bcc1941ce7
vasudevr <vasudevr@cybage.com>
2017-06-14 12:28:28 +0530

    1. Material spinner added. 2. Next button added

--- app/src/main/java/com/example/vasudevr/bi_sud/ui/view/fragment/BasicInformationFragment.java
+++ app/src/main/java/com/example/vasudevr/bi_sud/ui/view/fragment/BasicInformationFragment.java
@@ -6,14 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.example.vasudevr.bi_sud.R;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 /**
  * Created by vasudevr on 6/12/2017.
  */
 
 public class BasicInformationFragment extends Fragment {
+
+    private MaterialBetterSpinner mSpinner;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -24,6 +28,13 @@ public class BasicInformationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_basic_information, container, false);
+        mSpinner = (MaterialBetterSpinner) rootView.findViewById(R.id.selectState);
+        String[] list = getResources().getStringArray(R.array.month);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, list);
+
+        mSpinner.setAdapter(adapter);
         return rootView;
     }
 }
